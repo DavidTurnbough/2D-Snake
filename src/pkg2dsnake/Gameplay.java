a5fae03c9b877a48a23763173f71e0632c723d16
David <wdturnbo@uark.edu>
2020-05-04 12:14:06 -0500

    Added image files.

--- src/pkg2dsnake/Gameplay.java
+++ src/pkg2dsnake/Gameplay.java
@@ -2,6 +2,7 @@ package pkg2dsnake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
@@ -19,22 +20,21 @@ public class Gameplay extends JPanel
     @Override
     public void paint(Graphics g)
     {
-        
+        // Draw title image border
         g.setColor(Color.white);
         g.drawRect(24, 10, 851, 55);
         
-        titleImage = new ImageIcon("snaketitle.jpg");
+        // Draw title image.
+        titleImage = new ImageIcon("images/snaketitle.jpg");
         titleImage.paintIcon(this, g, 25, 11);
         
+        // Draw border for gameplay
         g.setColor(Color.white);
         g.drawRect(24, 74, 851, 577);
         
+        // Draw background for gameplay
         g.setColor(Color.black);
         g.fillRect(25, 75, 850, 575);
-        
-        g.setColor(Color.BLACK);
-        g.fillRect(25, 75, 850, 575);
-        
         
     }
 }
