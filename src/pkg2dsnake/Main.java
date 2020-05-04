8e9dec27e18b6c21cda2adfcbc57b1df55861796
David <wdturnbo@uark.edu>
2020-05-04 12:16:07 -0500

    Moved image files.

--- src/pkg2dsnake/Main.java
+++ src/pkg2dsnake/Main.java
@@ -1,3 +1,9 @@
+//******************************************************************************
+// Name: David Turnbough
+// Date: May 03, 2020 Sunday
+// YouTube: Java Tutorial How to Develop Game in Java (Snake Game)
+//https://www.youtube.com/watch?v=_SqnzvJuKiA
+//******************************************************************************
 package pkg2dsnake;
 
 import java.awt.Color;
@@ -5,15 +11,18 @@ import javax.swing.JFrame;
 
 public class Main
 {
+
     public static void main(String[] args)
     {
         JFrame myFrame = new JFrame();
+        Gameplay myGame = new Gameplay();
+
         myFrame.setTitle("2D-Snake");
         myFrame.setBackground(Color.DARK_GRAY);
-        myFrame.setBounds(10, 10, 500, 500);
+        myFrame.setBounds(10, 10, 905, 700);
         myFrame.setResizable(false);
         myFrame.setVisible(true);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        myFrame.add(myGame);
     }
 }
